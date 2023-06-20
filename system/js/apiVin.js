2b8f79f1ac93a6427453f776bc426e61112dc6a2
26Lunas <yiserret@misena.edu.co>
2023-06-20 16:41:44 -0500

    modificar-02421

---
+++ system/js/apiVin.js
@@ -0,0 +1,21 @@
+$(function(){
+    console.log("Jquery esta funcionando...")
+
+
+    $.ajax({
+        url: "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/",
+        type: "POST",
+        data: { format: "json", data: "3GNDA13D76S000000;5XYKT3A12CG000000;"},
+        dataType: "json",
+        success: function(result)
+        {
+            console.log(result);
+        },
+        error: function(xhr, ajaxOptions, thrownError)
+        {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    });
+        
+});
